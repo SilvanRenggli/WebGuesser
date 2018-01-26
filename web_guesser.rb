69df3075bd1949589678b904685d975f69491181
Silvan Renggli <sili.renggli@gmail.com>
2018-01-26 14:46:03 +0100

    add changing background color

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,18 +2,31 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 SECRET_NUMBER = rand(101)
+
 get '/' do
+  game = Game.new()
   guess = params[:guess].to_i
-  message = check_guess(guess)
-  erb :index, :locals => {:message => message}
+  game.check_guess(guess)
+  erb :index, :locals => {:message => game.message, :background_color => game.color}
 end
 
-def check_guess(guess)
-  if (guess > SECRET_NUMBER)
-    message = (guess - SECRET_NUMBER) > 5 ? "Way too High!": "Too High!"
-  elsif (guess < SECRET_NUMBER)
-    message = (SECRET_NUMBER - guess) > 5 ? "Way too Low!": "Too Low!"
-  else
-    message = "Correct! <br></br> The secret number is #{SECRET_NUMBER}!"
+class Game
+  attr_accessor :message, :color
+  @message
+  @color
+  def initialize
+
+  end
+  def check_guess(guess)
+    if (guess > SECRET_NUMBER)
+      @message = (guess - SECRET_NUMBER) > 5 ? "Way too High!": "Too High!"
+      @color = (guess - SECRET_NUMBER) > 5 ? "red": "LightCoral"
+    elsif (guess < SECRET_NUMBER)
+      @message = (SECRET_NUMBER - guess) > 5 ? "Way too Low!": "Too Low!"
+      @color = (SECRET_NUMBER - guess) > 5 ? "red": "LightCoral"
+    else
+      @message = "Correct! <br></br> The secret number is #{SECRET_NUMBER}!"
+      @color = "Lime"
+    end
   end
 end
